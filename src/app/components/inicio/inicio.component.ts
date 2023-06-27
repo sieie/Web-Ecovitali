eda95cc15f384bc3a0e37fde412daf1dd9edfb27
Prestto03 <sistemas.prestto03@gmail.com>
2023-06-27 17:06:33 -0500

    27-06

--- src/app/components/inicio/inicio.component.ts
+++ src/app/components/inicio/inicio.component.ts
@@ -1,62 +1,63 @@
-import { Component, OnInit, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { trigger, state, style, animate, transition } from '@angular/animations';
+import { trigger, style, animate, transition } from '@angular/animations';
+
+
+interface Image {
+  src: string;
+  alt: string;
+}
 
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.component.html',
   styleUrls: ['./inicio.component.css'],
   animations: [
-    trigger('reduceImage', [
-      state('initial', style({})),
-      state('final', style({})),
-      transition('initial => final', [
-        animate('5s')
-      ])
-    ])
-  ]
+    trigger('fadeinUp', [
+      transition(':enter', [
+        style({ opacity: 0, transform: 'translateY(0px)' }),
+        animate('1200ms ease', style({ opacity: 1, transform: 'translateY(0)' })),
+      ]),
+    ]),
+  ],
 })
-export class InicioComponent implements OnInit, AfterViewInit  {
-  estadoAnimacion: string;
+export class InicioComponent implements OnInit {
 
-  constructor(private router: Router ) {
-    this.estadoAnimacion = 'final';
+  //RULETA - ELLIPSE
+  images: Image[] = [
+    { src: 'SVG-Icono-Division-07.svg', alt: 'Control de Plagas' },
+    { src: 'SVG-Icono-Division-08.svg', alt: 'Limpieza y Desinfección' },
+    { src: 'SVG-Icono-Division-09.svg', alt: 'Prados y Jardines' },
+    { src: 'SVG-Icono-Division-10.svg', alt: 'Sanitización' }
+  ];
+
+  activeImageIndex = 0;
+  titulos = ['GESTION DE SERVICIOS', 'CONTROL DE PLAGAS', 'LIMPIEZA Y DESINFECCIÓN', 'SANITIZACIÓN', 'PRADOS Y JARDINES']
+  tituloActual = 'GESTION DE SERVICIOS';
+
+  changeActiveImageIndex(index: number) {
+    this.activeImageIndex = index;
+    this.tituloActual = this.titulos[this.activeImageIndex];
+  }
+
+  getImagePath(image: Image): string {
+    return `assets/img/website${image.src}`;
+  }
+
+
+  // CAMBIO DE TITULO CON EL BOTON DE LA DIVISION
+  ngOnInit() {
+    this.tituloActual = 'GESTION DE SERVICIOS';
+  }
+
+  cambiarTitulo(titulo: string) {
+    this.tituloActual = titulo;
   }
 
   darkMode: boolean = false;
   toggleDarkMode() {
     this.darkMode = !this.darkMode;
     document.body.classList.toggle('dark-mode');
-  }
-  @ViewChild('carouselElement', { static: false }) carouselElement!: ElementRef;
-  carouselItems = [
-    { src: './assets/img/website/Logo-Ecovitali-Gris.webp', alt: 'Imagen 1' },
-    { src: './assets/img/website/Logo-Ecovitali-Gris.webp', alt: 'Imagen 2' },
-    { src: './assets/img/website/Logo-Ecovitali-Gris.webp', alt: 'Imagen 3' }
-  ];
-  currentIndex = 0;
-
-  ngOnInit() {
-    setTimeout(() => {
-      this.estadoAnimacion = 'initial';
-      setTimeout(() => {
-        this.estadoAnimacion = 'final';
-      }, 2000);
-    });
-  }
-
-  ngAfterViewInit() {
-    this.startAnimation();
-  }
-
-  startAnimation() {
-    const carouselItems = this.carouselElement.nativeElement.querySelectorAll('.carousel-item');
-
-    carouselItems[this.currentIndex].addEventListener('animationend', () => {
-      carouselItems[this.currentIndex].classList.remove('active');
-      this.currentIndex = (this.currentIndex + 1) % carouselItems.length;
-      carouselItems[this.currentIndex].classList.add('active');
-    });
   }
 }
 
