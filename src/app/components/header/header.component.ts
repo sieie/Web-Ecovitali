b25b0a7cc74473ce18bd4d28267305d31ac1f31a
Prestto03 <sistemas.prestto03@gmail.com>
2023-09-14 12:26:19 -0500

    commit para conexion remota

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -3,9 +3,7 @@ import { Component, HostListener } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: [
-    './header.component.css',
-]
+  styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
   isNavbarColored = false;
--- src/app/components/inicio/inicio.component.ts
+++ src/app/components/inicio/inicio.component.ts
@@ -9,25 +9,25 @@ import { gsap } from 'gsap';
   styleUrls: ['./inicio.component.css'],
 })
 
-export class InicioComponent implements OnInit {
+export class InicioComponent {
 
   constructor(private router: Router) { }
 
-  ngOnInit(): void {
-      gsap.registerPlugin(ScrollTrigger);
+  // ngOnInit(): void {
+  //     gsap.registerPlugin(ScrollTrigger);
 
-      gsap.from('.animate', {
-        x: -100,
-        opacity: 0,
-        duration: 1,
-        scrollTrigger: {
-          trigger: '.animate',
-          start: 'top center',
-          end: 'bottom center',
-          markers: true,
-        },
-      });
-    }
+  //     gsap.from('.animate', {
+  //       x: -100,
+  //       opacity: 0,
+  //       duration: 1,
+  //       scrollTrigger: {
+  //         trigger: '.animate',
+  //         start: 'top center',
+  //         end: 'bottom center',
+  //         markers: true,
+  //       },
+  //     });
+  //   }
   }
   // // REDIRECION AL COMPONENTE SERVICIO
   // redirectToContacto() {
